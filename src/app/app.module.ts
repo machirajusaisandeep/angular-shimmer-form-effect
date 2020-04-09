dd9d054b37a6adab9848db2e9c3d278cb29cb002
machirajusaisandeep <machirajusaisandeep@gmail.com>
2020-04-09 17:31:02 +0530

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ShimmerComponent } from './shimmer/shimmer.component';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,MaterialModule,BrowserAnimationsModule ],
   declarations: [ AppComponent, HelloComponent, ShimmerComponent ],
   bootstrap:    [ AppComponent ]
 })
